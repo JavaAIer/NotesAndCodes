24ebf5f89d0426f430cf164c05629b0849ef5d89
cuteui@qq.com <cuteui@qq.com>
2019-05-27 01:17:14 +0800

    2019-5-27

--- java/springbootvue/c10/globalmethod/src/main/java/org/sang/security/globalmethod/HomeController.java
+++ java/springbootvue/c10/globalmethod/src/main/java/org/sang/security/globalmethod/HomeController.java
@@ -1,5 +1,6 @@
 package org.sang.security.globalmethod;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @Bean
+    @Autowired
     MethodService methodService;
 
 
@@ -22,11 +23,22 @@ public class HomeController {
     }
 
     @GetMapping("/dba/hello")
-    public String admin() {
+    public String dba() {
         return methodService.dba();
     }
 
+    @GetMapping("/methodadmin")
     public String SeeAdminMethod(){
-        return
+        return methodService.admin();
+    }
+
+    @GetMapping("/methoddba")
+    public String SeeDbaMethod(){
+        return methodService.dba();
+    }
+
+    @GetMapping("/methoduser")
+    public String SeeUserMethod(){
+        return methodService.user();
     }
 }
