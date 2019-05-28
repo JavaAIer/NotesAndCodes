11f7dbf7c9d0f057355c7ec32bac9ee5f1acd1db
javaaier <cuteui@qq.com>
2019-05-28 22:07:32 +0800

    2019年5月28日

--- java/springbootvue/c10/mysql01/src/main/java/org/sang/security/mysql01/config/CustomFilterInvocationSecurityMetadataSource.java
+++ java/springbootvue/c10/mysql01/src/main/java/org/sang/security/mysql01/config/CustomFilterInvocationSecurityMetadataSource.java
@@ -14,16 +14,35 @@ import org.springframework.util.AntPathMatcher;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Spring Security 中通过FilterInvocationSecurityMetadataSource接口中的
+ * getAttributes方法来确定一个请求需要哪些角色。
+ * FilterInvocationSecurityMetadataSource接口的默认实现类是：
+ * DefaultFilterInvocationSecurityMetadataSource，参考DefaultFilterInvocationSecurityMetadataSource的实现
+ * 开发者可以定义自己的FilterInvocationSecurityMetadataSource。
+ */
 @Component
 public class CustomFilterInvocationSecurityMetadataSource
         implements FilterInvocationSecurityMetadataSource {
+    //创建一个AntPathMatcher，主要用来实现ant风格的url匹配
     AntPathMatcher antPathMatcher = new AntPathMatcher();
     @Autowired
     MenuMapper menuMapper;
+
+    /**
+     * 主要实现该方法，参数是一个FilterInvocation，开发者可以从FilterInvocation中提取出当前
+     * 请求的URL，返回值是Collection<ConfigAttribute>，表示当前请求Url所需的角色。
+     * @param object
+     * @return
+     * @throws IllegalArgumentException
+     */
     @Override
     public Collection<ConfigAttribute> getAttributes(Object object)
             throws IllegalArgumentException {
+        //从参数中提取出当前请求的url。
         String requestUrl = ((FilterInvocation) object).getRequestUrl();
+        //从数据库中获取所有的资源信息，即本案例中的menu表以及menu所对应的role。
+        //在真实项目中，开发者可以将资源信息缓存在redis或者其他缓存数据库中。
         List<Menu> allMenus = menuMapper.getAllMenus();
         for (Menu menu : allMenus) {
             if (antPathMatcher.match(menu.getPattern(), requestUrl)) {
@@ -37,10 +56,12 @@ public class CustomFilterInvocationSecurityMetadataSource
         }
         return SecurityConfig.createList("ROLE_LOGIN");
     }
+
     @Override
     public Collection<ConfigAttribute> getAllConfigAttributes() {
         return null;
     }
+
     @Override
     public boolean supports(Class<?> clazz) {
         return FilterInvocation.class.isAssignableFrom(clazz);
