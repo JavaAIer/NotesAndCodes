19279d08ea5ad1de8abeff44c64d8386c7927c4e
cuteui@qq.com <cuteui@qq.com>
2019-06-26 17:54:57 +0800

    2019-6-26

---
+++ environment/docker/docker安装rabbitmq.md
@@ -0,0 +1,15 @@
+``` bash
+docker search rabbitmq
+docker pull rabbitmq
+
+```
+
+
+
+
+
+``` bash
+# 停止其他mq
+docker stop activemq01
+```
+
---
+++ java/springbootvue/c12/amqp-demo/src/main/java/com/javaaier/AmqpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javaaier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmqpDemoApplication.class, args);
+    }
+
+}
