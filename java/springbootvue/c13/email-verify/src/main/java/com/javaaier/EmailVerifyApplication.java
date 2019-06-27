6f28955647c19f5694889933f3d2618b6b55ee94
cuteui@qq.com <cuteui@qq.com>
2019-06-27 18:00:21 +0800

    2019-6-27

---
+++ java/springbootvue/c13/email-verify/src/main/java/com/javaaier/EmailVerifyApplication.java
@@ -0,0 +1,13 @@
+package com.javaaier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailVerifyApplication.class, args);
+    }
+
+}
---
+++ java/springbootvue/c13/email-verify/src/main/java/com/javaaier/MailService.java
@@ -0,0 +1,97 @@
+package com.javaaier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Component
+public class MailService {
+    /**
+     * JavaMailSender是在MailSenderPropertiesConfiguration中配置好的,该类在Mail自动配置类
+     * MailSenderAutoConfiguration中导入,因此这儿注入就可以使用了.
+     */
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void sendMailWithImg(String from, String to,
+                                String subject, String content,
+                                String[] srcPath,String[] resIds) {
+        if (srcPath.length != resIds.length) {
+            System.out.println("发送失败");
+            return;
+        }
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(message,true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);
+            for (int i = 0; i < srcPath.length; i++) {
+                FileSystemResource res =
+                        new FileSystemResource(new File(srcPath[i]));
+                helper.addInline(resIds[i], res);
+            }
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            System.out.println("发送失败");
+        }
+    }
+
+    /**
+     * 发送简单邮件
+     * @param from 发件人
+     * @param to 收件人
+     * @param cc 抄送
+     * @param subject 主题
+     * @param content 内容
+     */
+    public void sendSimpleMail(String from, String to, String cc, String subject, String content) {
+        SimpleMailMessage simpMsg = new SimpleMailMessage();
+        simpMsg.setFrom(from);
+        simpMsg.setTo(to);
+        simpMsg.setCc(cc);
+        simpMsg.setSubject(subject);
+        simpMsg.setText(content);
+        javaMailSender.send(simpMsg);
+    }
+
+    public void sendAttachFileMail(String from, String to,
+                       String subject, String content, File file) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message,true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content);
+            helper.addAttachment(file.getName(), file);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendHtmlMail(String from, String to,
+                             String subject, String content){
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setFrom(from);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            System.out.println("发送失败");
+        }
+    }
+}
